7ec30acfd11ff345f7a0c2f952d7be88c503e681
jmarcolan <jamarcolan@gmail.com>
2020-09-08 21:42:32 -0300

    Arrumando a api que envia os dados selecionados

--- deposito_watcher/api.py
+++ deposito_watcher/api.py
@@ -2,6 +2,9 @@
 Arquivo que tem a api para o etho_smart
 """
 import deposito_watcher.eto_mongo as mg
+from bson.objectid import ObjectId
+
+import deposito_watcher.fusao_variaveis as fus
 
 def get_usuario(usuario, senha):
     try:
@@ -51,3 +54,81 @@ def get_exp_by_hash(hash_experimento, usuario_id):
             return False, {}
     except:
         return False, {}
+    
+def get_csv_by_query(hash_experimento, dict_query):
+    try:
+        dict_template = constroi_a_query_inteira(hash_experimento, dict_query)
+        print(dict_template)
+
+        lis_de_juncao = ["sexo", "dosagem", "unidade"]
+        
+        fs = fus.Fusao_variaveis(dict_template)
+        fs.set_variaveis_juncao(lis_de_juncao)
+        li_str_descritores = ["duracao", "duracao_total", "path_experimento"]
+        li_str_categora = dict_query["categoria_comportamental"]
+
+        
+        fs.set_variaveis_descritore_eto_experimento(li_str_descritores, li_str_categora)
+        df = fs.get_dados_fundidos()
+
+        # fs = fus.Fusao_variaveis(dict_template)
+        # fs.set_variaveis_rastreamento(["@Vd", "@Van"])
+        # fs.set_variaveis_etografia(["nome", "trecho", "q_inicial", "q_final"])
+        # fs.set_variaveis_juncao(["sexo", "dosagem", "unidade"])
+        # df = fs.get_dados_fundidos()
+        return True, df
+    except:
+        return False, None
+
+   
+def constroi_a_query_inteira(hash_experimento, dict_query):
+    def constroi_query(key, lista, dict_query):
+        r_lista_sem_nada = len(lista) == 0 
+        r_tamanho_lista_maior_1 = len(lista) > 1
+        if r_tamanho_lista_maior_1:
+            dict_query = {"$or": []}
+            for elemento in lista:
+                dic_template = {key: elemento}
+                dict_query["$or"].append(dic_template)
+            return dict_query
+        else:
+            dict_query = {key: lista[0]}
+            return dict_query
+
+
+    
+
+    r_var_unidade = "unidadetodos" in  dict_query["var_ind.unidade"]
+    r_var_sexo = "sexotodos" in  dict_query["var_ind.sexo"]
+    r_var_dosagem = "dosagemtodos" in  dict_query["var_ind.dosagem"]
+
+
+    dict_unidade = {}
+    dict_sexo = {}
+    dict_dosagem = {}
+    dict_template = { "$and": [{"id_experimento": ObjectId(hash_experimento)}]}
+
+
+    if r_var_unidade:
+        pass
+    else:
+        dict_unidade = constroi_query("var_ind.unidade", dict_query["var_ind.unidade"],dict_query)
+        dict_template["$and"].append(dict_unidade)
+
+    if r_var_sexo:
+        pass
+    else:
+        dict_sexo = constroi_query("var_ind.sexo", dict_query["var_ind.sexo"], dict_query)
+        dict_template["$and"].append(dict_sexo)
+
+    if r_var_dosagem:
+        pass
+    else:
+        dict_dosagem = constroi_query("var_ind.dosagem", dict_query["var_ind.dosagem"], dict_query)
+        dict_template["$and"].append(dict_dosagem)
+    
+    return dict_template
+
+
+
+    
--- tests/test_api.py
+++ tests/test_api.py
@@ -12,3 +12,26 @@ def test_senha_errada():
 
 def test_get_list_experimento():
     r_saida, documentos = api.get_list_experimento("5f4f8f39c66cad3eb1dd0d4d")
+
+def test_get_dados():
+    dict_saida = {'Variaveis_cinematicas': ['Variaveis_cinematicastodos'],
+                   'Variaveis_etograficas': ['duracao_total'], 
+                   'categoria_comportamental': ['Immobility'], 
+                   'var_ind.dosagem': ['veiculo'], 
+                   'var_ind.sexo': ['macho'], 
+                   'var_ind.unidade': ['unidadetodos']}
+
+
+    id_experimento = '5f4f8f39c66cad3eb1dd0d50'
+    r_ecnotrn, df = api.get_csv_by_query(id_experimento, dict_saida)
+
+    
+
+    
+
+
+    # dict_query = { "$and": [{"id_experimento": ObjectId("5ea1a82993f4d56ba41e567d")}, 
+    #                     {"var_ind.sexo":"femea"},
+    #                     {"$or": [{"var_ind.dosagem": "flx2.5mg"},
+    #                              {"var_ind.dosagem": "veiculo"}
+    #                     ]}]}
